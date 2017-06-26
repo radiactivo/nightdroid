97ee481507cf4c38ee2f7ce1ceaf71a05e3fc49d
radiactivo <radiactivo@radiactivos-MacBook-Pro-2.local>
2017-06-26 03:13:01 +0200

    0.1.2

--- lib/interfaces/adb_iface.py
+++ lib/interfaces/adb_iface.py
@@ -1,45 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Nightmare Fuzzing Project generic fuzzer
+Generic application fuzzer
+@developer: wormer
+"""
+
+import sys
+from os import open, read, write
 from crash_data import CCrashData
+from tempfile import mkstemp
+import threading
+from nfp_log import log
+sys.path.append("../")
+from utils import run_subproc, flush_log
 
 
-class ADBInterface():
-	def __init__(self, log_pipe, ):
-		pass
+class ADBInterface(object):
+	def __init__(self, logfile, cmd, device_id):
+		self.logfile = logfile
+		self.cmd = cmd
+		self.device_id = device_id
+		self.crash_data = CCrashData()
+		self.running = False
+		self.cmd_start_log = 'adb -s %s shell log -p W -t Fuzz Starting', self.device_id
+		self.cmd_finish_log = 'adb -s %s shell log -p W -t Fuzz Finishing', self.device_id
+		self.cmd_pull_tombstone = 'adb -s %s shell', self.device_id
 
 	def clear_log():
+		flush_log(self.device_id)
 
-	def parse_logs():
-		recording = False
-		crash = None
-		buff = []
-		while 'W Fuzz    : Starting' not in fd.readline():
-			pass
+	def parse_tombstone(tombstone):
+		with open(tombstone, 'r+') as fd:
+			text = fd.read()
+			for line in text.split('\n'):
+				if 'eip' in line:
+					pass
 
-		while l != 'W Fuzz    : Finishing':
-			l = fd.readline()
-			buff.append(l)
-		
-		if crash:
-			recover_tombstone()
-			clean_tombstone()
-
+	def parse_log():
+		while self.running:
+			with open(self.logfile, 'r+') as f:
+				l = fd.readline()
+				if 'Fatal signal' in l:
+					pass
 	def dump_log():
-		pass	
+		run_subproc('adb - s %s logcat >> %s' % (self.device_id, self.logfile))
+		
 	def run():
 		# Log starting
 		# ./adb shell log -p W -t Fuzz Starting
-		pass
+		self.running = True
+		t_log = threading.Thread(target=dump_log, args=[])
+    	t_log.start()
+ 		t_parser = threading.Thread(target=parse_log, args=[])
+		run_subproc(cmd_start_log)
+		run_subproc(self.cmd)
+		run_subproc(cmd_finish_log)
+		t_log.join()
+		return None
 
 #-----------------------------------------------------------------------
-def main(args):
+def main(args, cmd=None, device_id=None):
 	logfile = mkstemp()[1]
-	adb = ADBInterface()
-	return adb.run()
+	adb = ADBInterface(logfile, cmd, device_id)
+	data = adb.run()
+	os.remove(logfile)
+	return data
 
 #-----------------------------------------------------------------------
 def usage():
 	print "Usage:", sys.argv[0], "adb <command>"	
 
-if __mame__ == '__main__':
+if __name__ == '__main__':
 	if len(sys.argv) == 1:
     	usage()
   	else:
---
+++ lib/utils.py
@@ -0,0 +1,24 @@
+from os import listdir
+import os
+import sys
+import subprocess
+import re
+import time
+import fnmatch
+
+def run_subproc(cmd):
+    r = subprocess.Popen([cmd], shell=True)
+    r.wait()
+
+def flush_log(device_id):
+    cmd = 'adb -s ' + (str)(device_id) + ' logcat -c'
+    r = subprocess.Popen([cmd], shell=True)
+    r.wait()
+
+def find(pattern, path):
+    result = [] 
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                result.append(os.path.join(root, name))
+    return result
